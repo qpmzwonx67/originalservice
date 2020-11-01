f9b6bbdee1ddf4cc2d97391f6308ad1d025e4f9b
guzuri-nu <qpmz.wonx67@gmail.com>
2020-11-01 01:48:25 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Child's Safariへようこそ</h1>
+    @if (Auth::check())
+        {{  Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Child's Safariへようこそ</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', '新規登録はこちら', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
